a1b8fe561377d9cba7ae784a13325643b9077221
Deh Oliveira <sihann@outlook.com.br>
2021-05-01 10:12:50 -0300

    Adicionando componente Results(resultado de busca)

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+import { ProductModal } from '../ProductModal';
 import { SingleProduct } from '../SingleProduct';
 import { Container, Content } from './styles';
 
 export function Results() {
+  const [modalProcuctOpen, setModalProductOpen] = useState(false);
+
+  function clickOpenModalProduct() {
+    setModalProductOpen(true)
+  }
+
+  function clickCloseModalProduct() {
+    setModalProductOpen(false)
+  }
+  
   return (
     <Container>
       <Content>
         <span>1-48 de mais de 8.000 resultados para <strong>"notebook samsung"</strong></span>
         <hr/>
-        <SingleProduct/>
+        <SingleProduct onOpenModalProduct={clickOpenModalProduct}/>
+        <ProductModal isOpen={modalProcuctOpen} onRequestClose={clickCloseModalProduct}/>
       </Content>
     </Container>
   )
