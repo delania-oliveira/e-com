0e642c63c99dc8383524cb9c32fb9b5e9f7f5f9e
Deh Oliveira <sihann@outlook.com.br>
2021-05-01 10:14:16 -0300

    Adicionando ProductModal ao SingleProduct

--- src/components/SingleProduct/index.tsx
+++ src/components/SingleProduct/index.tsx
@@ -1,10 +1,14 @@
 import { Container, Content } from './styles';
 import productImg from '../../assets/product.png';
 
-export function SingleProduct() {
+interface SingleProps {
+  onOpenModalProduct: () => void;
+}
+
+export function SingleProduct({onOpenModalProduct}: SingleProps) {
   return (
     <Container>
-      <Content>
+      <Content onClick={onOpenModalProduct}>
         <img src={productImg} alt="Notebook Samsung Book X30 10ª Intel Core I5 8GB 1TB W10 15,6'' Prata"/>
         <p>Notebook Samsung Book X30 10ª Intel Core I5 8GB 1TB W10 15,6'' Prata</p>
         <h3>R$ 3.999,99</h3>
