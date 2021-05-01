93092ac76c359d600459b81a00708cb6f86c2096
Deh Oliveira <sihann@outlook.com.br>
2021-05-01 10:44:51 -0300

    Adicionando a página Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from '../../pages/Home/styles';
+
+export default function Home() {
+  return (
+    <Container>
+      <span>Se der tempo um carousel aqui</span>
+    </Container>
+  )
+}
