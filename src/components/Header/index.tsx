4a95e9c903a6995dc90686078300517f3fc3c136
Deh Oliveira <sihann@outlook.com.br>
2021-05-03 17:52:54 -0300

    implementando busca

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,29 @@
+import React, { useState } from 'react';
 import { Container, Content} from './styles';
+import { useHistory } from 'react-router-dom';
+
 
 export function Header() {
+  const [search, setSearch] = useState<string>();
+  const history = useHistory()
+  
+  function handleSearch(e:React.FormEvent) {
+    e.preventDefault()
+    history.push(`/search-result/${search}`)
+  } 
+
+
   return (
     <Container>
       <Content>
         <h1>e-com</h1>
-        <form action="/" method="GET">
-          <input type="text" name="search" placeholder="O que você está procurando?" value="search"/>
+        <form onSubmit={handleSearch}>
+          <input 
+            type="text" 
+            name="search" 
+            placeholder="O que você está procurando?" 
+            value={search} onChange={e => setSearch(e.target.value)}
+          />
           <button type="submit"><i className="fas fa-search"></i></button>
         </form>   
       </Content>
