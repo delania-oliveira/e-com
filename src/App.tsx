8ef2d9d246aef79a61d933b22b6f683bbc87778b
Deh Oliveira <sihann@outlook.com.br>
2021-05-03 17:17:52 -0300

    fix App

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
 import { Routes } from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
 
 Modal.setAppElement('#root');
 
 export function App() {
+
   
   return (
     <div className="App">
-      <Header/>
-      <Routes/>
+      <BrowserRouter>
+        <Header/>
+        <Routes/>
+      </BrowserRouter>
       <GlobalStyles/>
     </div>
   );
