e8a76054088fd2ebf4cd117ba66d9a795eec37b6
Deh Oliveira <sihann@outlook.com.br>
2021-05-03 17:01:05 -0300

    implementando resultado de busca

--- src/pages/SearchResult/index.tsx
+++ src/pages/SearchResult/index.tsx
@@ -1,12 +1,49 @@
 import { Sidebar } from '../../components/Sidebar'
 import { Results } from '../../components/Results'
 import { Container } from '../../pages/SearchResult/styles';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+import { RouteComponentProps } from 'react-router';
 
-export default function SearchResult() {
+interface ResultProps {
+    productId: string;
+    productName: string;
+    description: string;
+    items: [ 
+      {
+      images: [{imageUrl: string}];
+      sellers: [{
+        commertialOffer: {
+          Installments: [{
+             Value: number 
+          }]}}]}
+    ];
+  }
+
+  interface MatchParams {
+    search: string;
+  }
+  interface Props extends RouteComponentProps<MatchParams> {
+  }
+
+export default function SearchResult(props: Props) {
+ const [results, setResults] = useState<ResultProps[]>([]);
+
+  useEffect(()=>{
+    const search = props.match.params.search
+    console.log(search)
+    api.get(`api/catalog_system/pub/products/search/${search?search:""}`)
+      .then(response => {
+         if(response.data) { 
+            setResults(response.data)
+          }
+    })
+  } ,[props])
+
   return (
     <Container>
         <Sidebar/>
-        <Results/>
+        <Results results={results}/>
     </Container>
   )
 }
