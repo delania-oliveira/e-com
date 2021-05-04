abdeabc901f40dbc8bed990a756dbc2cf185b74b
Deh Oliveira <sihann@outlook.com.br>
2021-05-04 18:52:16 -0300

    fix componente

--- src/components/AutocompleteModal/index.tsx
+++ src/components/AutocompleteModal/index.tsx
@@ -14,18 +14,20 @@ interface modalSearchProps {
 }
 
 export function AutocompleteModal({isOpen, onRequestClose, autoCompleteResult}: modalSearchProps) {
- 
 
-  if (autoCompleteResult?.length>3) 
-    return (
-      <Modal isOpen={true}   onRequestClose={onRequestClose} overlayClassName="react-modal-native-autocomplete" className="react-modal-content-autocomplete">
-        <button type="button" onClick={onRequestClose} className="react-modal-close-autocomplete"> 
+  return (
+    <Modal isOpen={isOpen}   onRequestClose={onRequestClose} overlayClassName="react-modal-native-autocomplete" className="react-modal-content-autocomplete">
+        
+        <button type="button" onClick={onRequestClose} className="react-modal-close"> 
           <i className="fas fa-times"/>
         </button>
       <Container>
-          {autoCompleteResult?<li><a href={autoCompleteResult[0].href}>{autoCompleteResult[0].name}</a></li>: <></>}           
+        {autoCompleteResult.map(item => {
+          return (
+            <li><a href={item.href}>{item.name}</a></li>
+          )
+          })}           
         </Container>
       </Modal>
-   ) 
-   else return <></>
+  )
 }
