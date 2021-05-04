44eb0921810f5950cf043a263abbcfb31a1739db
Deh Oliveira <sihann@outlook.com.br>
2021-05-04 16:34:23 -0300

    implementando autocomplete ao Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,47 @@
 import React, { useState } from 'react';
 import { Container, Content} from './styles';
+import { AutocompleteModal } from '../AutocompleteModal';
 import { useHistory } from 'react-router-dom';
+import { api } from '../../services/api';
+
 
 
 export function Header() {
   const [search, setSearch] = useState<string>();
   const history = useHistory()
+  const [modalProcuctOpen, setModalProductOpen] = useState(false);
+  const [autoCompleteResult, setAutoCompleteResult] = useState([] as any);
   
+
   function handleSearch(e:React.FormEvent) {
     e.preventDefault()
     history.push(`/search-result/${search}`)
   } 
 
+  function handleSearchInput(value:string) {
+    if(value.length>3) {
+      api.get(`buscaautocomplete/?productNameContains=${value}`)
+      .then(response => {
+         if(response.data) { 
+          
+            setAutoCompleteResult(response.data.itemsReturned)
+            console.log(response.data.itemsReturned)
+          }
+    })
+    }
+    
+    return (
+      setSearch(value)
+    ) 
+  }
+  
+  function clickOpenModalProduct() {
+    setModalProductOpen(true)
+  }
+
+  function clickCloseModalProduct() {
+    setModalProductOpen(false)
+  }
 
   return (
     <Container>
@@ -22,10 +52,12 @@ export function Header() {
             type="text" 
             name="search" 
             placeholder="O que você está procurando?" 
-            value={search} onChange={e => setSearch(e.target.value)}
+            value={search} onChange={e => handleSearchInput(e.target.value)}
           />
           <button type="submit"><i className="fas fa-search"></i></button>
-        </form>   
+        </form>
+        
+        {autoCompleteResult?<AutocompleteModal autoCompleteResult={autoCompleteResult} isOpen={modalProcuctOpen} onRequestClose={clickCloseModalProduct}/>:<></>}
       </Content>
     </Container>
   )
